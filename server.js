66dbffa7d03f2611a9b30e3cf7c3ddb2cefb149e
Marcel Wrzecionek <wrzeciom@sheridancollege.ca>
2025-11-14 11:06:43 -0500

    Trigger workflow

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Dockerized Node.js App!');
+  res.send('(Updated Version) Hello, Dockerized Node.js App!');
 });
 
 app.listen(PORT, () => {
